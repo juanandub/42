09bcd6ab20dcd39eb98637c12affbc2ad74effb9
Juan Andres Pareja Ballesteros <jpareja-@c3r7s4.42malaga.com>
2024-12-19 13:14:28 +0100

    update: ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,50 +6,49 @@
 /*   By: jpareja- <jpareja-@student.42malaga.c>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 13:57:27 by jpareja-          #+#    #+#             */
-/*   Updated: 2024/12/19 11:50:13 by jpareja-         ###   ########.fr       */
+/*   Updated: 2024/12/19 13:11:38 by jpareja-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_aux(int aux)
+static int	ft_aux(int aux, int n)
 {
 	int	len;
 
-	len = 0;
-	while (aux > 0)
+	len = 1;
+	while (aux /= 10)
 	{
-		aux = aux / 10;
 		len++;
 	}
+	if (n < 0)
+		len++;
 	return (len);
 }
 
 char	*ft_itoa(int n)
 {
 	int	len;
-	int	aux;
-
+	char *result;
+	
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
-	if (n == 2147483647)
-		return (ft_strdup("2147483647"));
 	if (n == 0)
 		return (ft_strdup("0"));
-	aux = n;
-	len = ft_aux(aux);
-	char *result = malloc(len + 1 + (n < 0));
+	len = ft_aux(n, n);
+	result = malloc(len + 1);
 	if (!result)
 		return (NULL);
-	result[len + (n < 0)] = '\0';
-	if (n < 0)
+	result[len] = '\0';
+	if (n < 0){
 		result[0] = '-';
-	aux = len;
-	while (len--)
+		n = -n;
+	}
+	len--;
+	while (n)
 	{
-		result[aux] = n % 10 + '0';
+		result[len--] = n % 10 + '0';
 		n /= 10;
-		aux--;
 	}
 	return (result);	
 }
