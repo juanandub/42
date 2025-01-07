e7add508da4321ab8c1138d2a1b15816a911251a
Juan Andres Pareja Ballesteros <jpareja-@c3r8s4.42malaga.com>
2025-01-07 17:36:43 +0100

    mandatory part ok

---
+++ test.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpareja- <jpareja-@student.42malaga.c>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 17:28:03 by jpareja-          #+#    #+#             */
+/*   Updated: 2025/01/06 20:14:47 by jpareja-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+#include <stdio.h>
+
+int main(void)
+{
+	ft_printf("--------EMPIEZA EL TEST--------");
+	ft_printf("\nELEMENTO CHAR - PRIMERO MIO LUEGO ORIGINAL\n");
+	char c = 45;
+	int res = ft_printf("%c ", c);
+	int res1 = printf("%c ", c);
+	printf("\nres: %i, res1: %i",res,res1);
+	printf("\n");
+	res = ft_printf("%c ", 86);
+	res1 = printf("%c ", 86);
+	printf("\n");
+	ft_printf("res: %i, res1: %i",res,res1);
+	printf("\n");
+	ft_printf("\n--------------ENTEROS-----------\n");
+	int i = 123;
+	res = ft_printf("%d ", i);
+	res1 = printf("%d ", i);
+	printf("\nres: %i, res1: %i",res,res1);
+	printf("\n");
+	res = ft_printf("%u ", -1);
+	res1 = printf("%u ", -1);
+	printf("\n");
+	ft_printf("res: %i, res1: %i",res,res1);
+	printf("\n");
+	ft_printf("\n--------------VOID-----------\n");
+	int x = 42;
+	res = ft_printf("%p ", &x);
+	res1 = printf("%p ", &x);
+	printf("\n");
+	ft_printf("res: %i, res1: %i",res,res1);
+	printf("\n");
+}
