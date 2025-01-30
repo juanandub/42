f45beb98f00895562e8ffde7a6df7b5334776211
juanandub <juanandub@outlook.es>
2025-01-30 18:14:43 +0100

    update getnextline

--- getnextline/get_next_line.c
+++ getnextline/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jpareja- <jpareja-@student.42malaga.c>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 21:01:40 by jpareja-          #+#    #+#             */
-/*   Updated: 2025/01/27 13:48:52 by jpareja-         ###   ########.fr       */
+/*   Updated: 2025/01/30 18:14:08 by jpareja-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,36 +28,46 @@ static int	find_newline(const char *c)
 	return (-1);
 }
 
-char	*get_next_line(int fd) //
+static void	ft_aux(char **stash, char *buffer, int fd)
 {
-	static char	*stash;
-	char		*buffer;
 	char		*line;
 	ssize_t		b_read;
 	char		*tmp;
-
-	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (BUFFER_SIZE <= 0 || fd < 0 || read(fd, 0, 0) < 0)
-	{
-		free(buffer);
-		return (0);
-	}
-	if (!buffer)
-		return (0);
+	
 	while (find_newline(stash) == -1)
 	{
 		b_read = read(fd, buffer, BUFFER_SIZE);
 		if (b_read <= 0)
-			break;
+			return (0);
 		buffer[b_read]='\0';
-		if (!stash)
-			stash = ft_strdup(buffer);
+		if (!*stash)
+			*stash = ft_strdup(buffer);
 		else
 		{
-			tmp = ft_stjoin(stash, buffer);
-			free(stash);
-			stash = tmp;
+			tmp = ft_stjoin(*stash, buffer);
+			free(*stash);
+			*stash = tmp;
 		}
 	}
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*buffer;
+	int			i;
+	
+	stash = NULL;
+	if (BUFFER_SIZE <= 0 || fd < 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	ft_aux(&stash, buffer, fd);
+	free(buffer);
+	if (!stash || *stash == '\0')
+		return (NULL);
+	i = find_newline(stash);
 	return (0);
 }
